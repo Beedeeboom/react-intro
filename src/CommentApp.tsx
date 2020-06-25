c9b2e53d6d0a49560d1dafe2e81d1f37e5fdbe55
Beedeeboom <stephanie.duffieux@gmail.com>
2020-06-25 11:55:27 +1000

    adding map method to iterate over JSON object

--- src/CommentApp.tsx
+++ src/CommentApp.tsx
@@ -3,37 +3,20 @@ import "./App.scss"
 import Comment from "./Comment"
 import faker from "faker"
 import Card from "./Card"
+import comments from "./comments.json"
 
 export default (): JSX.Element => {
+    console.log(comments)
     return (
         <div className="comments">
-            <Card>
-            <Comment
-                author="Mary Smith"
-                avatar={faker.image.avatar()}
-                date="11/12 6:00pm"
-                text="I think you are really cool!"
-            />
-            </Card>
-            <Card>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, fugiat? Explicabo porro unde ipsum quae illo optio aliquam repellat blanditiis.</p>
-            </Card>
-            <Card heading="Last Comment!">
-            <Comment
-                // author="Jude Henry"
-                avatar={faker.image.avatar()}
-                date="09/12 1:30pm"
-                text="Hey this is great!"
-            />
-            </Card>
-            <Card>
-            <Comment
-                author="Bobby Miller"
-                avatar={faker.image.avatar()}
-                date="07/12 9:10am"
-                text="banana"
-            />
-            </Card>
+            {comments.map(comment =>
+                <Card>
+                    <Comment
+                        avatar={faker.image.avatar()}
+                        {...comment}
+                    />
+                </Card>
+        )}
         </div>
-    )
+    )    
 }
